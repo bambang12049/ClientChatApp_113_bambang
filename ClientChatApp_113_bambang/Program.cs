377725f513bf6985b7f3d19e00961b8ee17898fc
Bambang Marsudi <bambang.marsudi.ft18@mail.umy.ac.id>
2021-01-14 14:24:47 +0700

    Add project files.

---
+++ ClientChatApp_113_bambang/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientChatApp_113_bambang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InstanceContext context = new InstanceContext(new ClientCallback());
+            ServiceReference1.ServiceCallbackClient server = new ServiceReference1.ServiceCallbackClient(context);
+
+            Console.WriteLine("Enter username");
+            string nama = Console.ReadLine();
+            server.gabung(nama);
+
+            Console.WriteLine("Kirim pesan");
+            string pesan = Console.ReadLine();
+
+            //memeriksa apakah pesannya null
+            while (true)
+            {
+                if (!string.IsNullOrEmpty(pesan))
+                    server.kirimPesan(pesan);
+                Console.WriteLine("Kirim Pesan");
+                pesan = Console.ReadLine();
+            }
+        }
+    }
+    public class ClientCallback : ServiceReference1.IServiceCallbackCallback
+    {
+        public void pesanKirim(string user, string pesan)
+        {
+            Console.WriteLine("{0} : {1}", user, pesan);
+        }
+    }
+}
